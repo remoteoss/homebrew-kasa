ef23b6aa9ccda571b12f495aeb97dcc7815224c3
kasa CI <ci@kasa>
2026-05-29 06:36:06 +0000

    Update git-kasa to 0.2.1

--- Formula/git-kasa.rb
+++ Formula/git-kasa.rb
@@ -2,25 +2,25 @@ class GitKasa < Formula
   desc "Stacked Merge Requests CLI for GitLab"
   homepage "https://gitlab.com/remote-com/employ-starbase/kasa"
   license "MIT"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.0/git-kasa-darwin-arm64.tar.gz"
-      sha256 "0fb6812329f53185c96d58f92e114ec973196ae7ec16c6d2cda1ec91c4a27e0f"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.1/git-kasa-darwin-arm64.tar.gz"
+      sha256 "8edd90c46860268ccaf2c9c21418882537ec3eebdba9bf8e38e02ad768425def"
     else
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.0/git-kasa-darwin-x64.tar.gz"
-      sha256 "e88a707e7692e570f1920979d69634c84e1a563f9d7a335562ab8c6778b5dc7e"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.1/git-kasa-darwin-x64.tar.gz"
+      sha256 "e55205de8b0320f1f4bb6875c241fd0dbb5debaa8925e8cca654f788bacc3452"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.0/git-kasa-linux-arm64.tar.gz"
-      sha256 "8a0c88c86253b9fdf3ecb57f23b8fae3ec9819f2c14e53bb13c493602bd76915"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.1/git-kasa-linux-arm64.tar.gz"
+      sha256 "3c2713023a4b3fe45b17e79e88e2f71f8909ebf33541fbdaaad15bfcbfafa877"
     else
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.0/git-kasa-linux-x64.tar.gz"
-      sha256 "cb08c948cdefcd1a611a1862554ce8c12efa21c5580c986def61e29757442360"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.1/git-kasa-linux-x64.tar.gz"
+      sha256 "17ffb3f118b18262ae4c1165864691cba95cbed4877c8ed6c487267e4c845d65"
     end
   end
 
