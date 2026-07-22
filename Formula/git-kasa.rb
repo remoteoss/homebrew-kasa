3088a653eded7f3a9c59fb5729f1d775c93aa9f6
kasa CI <ci@kasa>
2026-07-22 09:20:45 +0000

    Update git-kasa to 0.2.4

--- Formula/git-kasa.rb
+++ Formula/git-kasa.rb
@@ -2,25 +2,25 @@ class GitKasa < Formula
   desc "Stacked Merge Requests CLI for GitLab"
   homepage "https://gitlab.com/remote-com/employ-starbase/kasa"
   license "MIT"
-  version "0.2.3"
+  version "0.2.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.3/git-kasa-darwin-arm64.tar.gz"
-      sha256 "3d114c4c4f973dfb976e305ea05e530732cc487a04b3fc3f4b6260307e2314de"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.4/git-kasa-darwin-arm64.tar.gz"
+      sha256 "064ccc9ce034ee7b7e08c6c5a2a413a65d4fc6f02e1f38bebccc65b5cf40638d"
     else
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.3/git-kasa-darwin-x64.tar.gz"
-      sha256 "04b09f9a3f0d32f3bacf09d03500b91a021e8a6f57bac56d2fa6526b8ebce1b7"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.4/git-kasa-darwin-x64.tar.gz"
+      sha256 "8c66f7aa08f2c32f0fe592fbb090b7046e54f64b56bf8838c7ac9e9e28a53fc0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.3/git-kasa-linux-arm64.tar.gz"
-      sha256 "dcff385bac4937e90c9370a5f3b7359983c325204e180decbe86d590e2701d7d"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.4/git-kasa-linux-arm64.tar.gz"
+      sha256 "76ca6f2aa31dfea59c94fafc93aafc07a08efd5bbcf79213b14e936804319e3e"
     else
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.3/git-kasa-linux-x64.tar.gz"
-      sha256 "a2c73305266ec02bfe62964125b2764662ad09055cae044eb4c90a5f6a6268cb"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.4/git-kasa-linux-x64.tar.gz"
+      sha256 "cfd57e8bfd64ab6c59427b5bd2f9b48bd1d561f82fbe12821ed84ad86a7c9b2b"
     end
   end
 
