f9621d5d1c6aadef7d7990d269332f9f9c9e7e20
kasa CI <ci@kasa>
2026-07-23 16:19:19 +0000

    Update git-kasa to 0.2.9

--- Formula/git-kasa.rb
+++ Formula/git-kasa.rb
@@ -2,25 +2,25 @@ class GitKasa < Formula
   desc "Stacked Merge Requests CLI for GitLab"
   homepage "https://gitlab.com/remote-com/employ-starbase/kasa"
   license "MIT"
-  version "0.2.8"
+  version "0.2.9"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.8/git-kasa-darwin-arm64.tar.gz"
-      sha256 "a7ef679526529486ed76f31031ab3376f19521ca0a3780000af179f0c5b5fb79"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.9/git-kasa-darwin-arm64.tar.gz"
+      sha256 "7189c8331dbeb70df030e58c059c278a98e08bb4ad3ef59d2ef2a83346be19f5"
     else
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.8/git-kasa-darwin-x64.tar.gz"
-      sha256 "44e6f0e75321a2bf6029b11e3a64dfbfe6abdb1ed0d7fbb78055b98d7b095422"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.9/git-kasa-darwin-x64.tar.gz"
+      sha256 "4f8f30224529643f9a9858cab4e72c5906f177dd82a51f17d212b23edd340087"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.8/git-kasa-linux-arm64.tar.gz"
-      sha256 "9ecdf9d400b61fbb33f8b1505182b448d4cc4c6430834b2e76262644570e28c0"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.9/git-kasa-linux-arm64.tar.gz"
+      sha256 "960f2628c58b9278c96daa8a0e312575cc99218eaf6219527cd7506a3e649cdf"
     else
-      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.8/git-kasa-linux-x64.tar.gz"
-      sha256 "0ad9ae30353b70c6583d4962a5a55f26a4943c78d3c06acfc04b3c068ff83a54"
+      url "https://gitlab.com/api/v4/projects/81261636/packages/generic/git-kasa/0.2.9/git-kasa-linux-x64.tar.gz"
+      sha256 "a247faef9fb474d427e4002e6545f8c9f24204e8c99b6f7b6365c06bc321cda1"
     end
   end
 
